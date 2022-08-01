10ae706f9c19578b3308ad4e691f648b0677e381
oktonion <0oktonion0@gmail.com>
2022-08-01 22:59:43 +0300

    Update runtime_reflection.cpp

--- tests/runtime_reflection.cpp
+++ tests/runtime_reflection.cpp
@@ -8,8 +8,45 @@
 TEST_SUITE("runtime reflection")
 {
 
-
 TEST_CASE("reflect some types"){
+    using namespace cppmeta;
+
+    int some_data_rt = tools::rand();
+    const int some_data_rt_const = tools::rand();
+
+    reflect<int>::
+        objects += 
+              object("some_data_rt", &some_data_rt)
+            , object("some_data_rt_ref", some_data_rt)
+            , object("some_data_rt_const", some_data_rt_const)
+        ,values +=
+            constant("max_int", 2048)
+        ;
+    
+    SUBCASE("resolving object by value")
+    {
+        int some_data_rt_resolved =
+            resolve<int>::object("some_data_rt").value;
+        const int& some_data_rt_const_resolved =
+            resolve<int>::object("some_data_rt_const").value;
+
+        CHECK(some_data_rt_resolved == some_data_rt);
+        CHECK(some_data_rt_const_resolved == some_data_rt_const);
+    }
+
+    SUBCASE("resolving object by const value")
+    {
+        const int& some_data_rt_resolved =
+            resolve<const int>::object("some_data_rt").value;
+        const int& some_data_rt_const_resolved =
+            resolve<const int>::object("some_data_rt_const").value;
+
+        CHECK(some_data_rt_resolved == some_data_rt);
+        CHECK(some_data_rt_const_resolved == some_data_rt_const);
+    }
+}
+
+TEST_CASE("reflect some class"){
     using namespace cppmeta;
 
     typedef tools::type_for_reflection type_for_rt_reflection;
@@ -33,5 +70,7 @@ TEST_CASE("reflect some types"){
         CHECK(obj.data1 == obj.*data1_tmp); // true
     }
 }
+
+
 
 }
